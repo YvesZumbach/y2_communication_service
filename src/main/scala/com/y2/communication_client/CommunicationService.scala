38eda90c64b1cda7ba145cbd5ac1db6bb89f7c24
Yves Zumbach <yzumbach@andrew.cmu.edu>
2019-04-18 10:25:28 -0400

    Add doc for CommunicationService

--- src/main/scala/com/y2/communication_client/CommunicationService.scala
+++ src/main/scala/com/y2/communication_client/CommunicationService.scala
@@ -2,8 +2,9 @@ package com.y2.communication_client
 
 import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props }
 
-
-
+/**
+  * Service that handles communication in the y2 cluster.
+  */
 class CommunicationService {
 
 }
