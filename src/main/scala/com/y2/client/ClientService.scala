20acbe2d1aca56cbcf4803194a7663356c678787
Yannick Bloem <yannickbloem@hotmail.com>
2019-04-18 10:36:04 -0400

    Added cluster-tools import and setup basic client service

---
+++ src/main/scala/com/y2/client/ClientService.scala
@@ -0,0 +1,21 @@
+package com.y2.client
+
+import akka.actor.{Actor, ActorLogging, ActorPath, ActorSystem, Props}
+import akka.cluster.client.{ClusterClient,ClusterClientSettings}
+import com.typesafe.config.ConfigFactory
+
+object ClientService {
+  def main(args: Array[String]): Unit = {
+    val config = ConfigFactory.parseString("akka.remote.netty.tcp.port=4551").withFallback(ConfigFactory.load())
+    val system = ActorSystem("ScalaClusterClientSystem", config)
+
+    val initialContacts = Set(
+      ActorPath.fromString("akka.tcp://ClusterSystem@127.0.0.1:2551/user/receptionist"),
+      ActorPath.fromString("akka.tcp://ClusterSystem@127.0.0.1:2552/user/receptionist"))
+    val c = system.actorOf(ClusterClient.props(
+      ClusterClientSettings(system).withInitialContacts(initialContacts)), "os-client")
+
+    c ! ClusterClient.Send("/user/serviceA", "hello", localAffinity = true)
+    c ! ClusterClient.SendToAll("/user/serviceB", "hi")
+  }
+}
