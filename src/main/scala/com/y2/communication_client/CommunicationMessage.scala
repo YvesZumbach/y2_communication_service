6893f294fdea44fee848918f7ef85aa25e5c6e36
Yves Zumbach <yzumbach@andrew.cmu.edu>
2019-04-18 10:36:40 -0400

    Add case class for NeuralNetworkDeltas

--- src/main/scala/com/y2/communication_client/CommunicationMessage.scala
+++ src/main/scala/com/y2/communication_client/CommunicationMessage.scala
@@ -3,6 +3,6 @@ package com.y2.communication_client
 /**
   * Represent all messages exchanged between communication services.
   */
-class CommunicationMessage {
+class CommunicationMessage { }
 
-}
+case class NeuralNetworkDeltas(deltas: Iterable[Int]) extends CommunicationMessage
