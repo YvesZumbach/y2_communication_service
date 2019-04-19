7df65529c5cf1f885c661548c4f065e3a95714a0
Yannick Bloem <yannickbloem@hotmail.com>
2019-04-18 22:25:34 -0400

    Updated version

--- src/main/scala/com/y2/runtype/Y2Node.scala
+++ src/main/scala/com/y2/runtype/Y2Node.scala
@@ -1,22 +1,35 @@
 package com.y2.runtype
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import akka.actor.{ActorRef, ActorSystem, Props}
-import com.typesafe.scalalogging.Logger
+import com.typesafe.config.{Config, ConfigFactory}
 import com.y2.communication_service.CommunicationService
-import com.y2.config.Config
+import com.y2.config.Y2Config
 
+/**
+  * Run a node.
+  * @param config the configuration to apply to the node.
+  */
+class Node(val config: Y2Config) {
 
-class NodeRun(val nodeConfig: Config)(implicit system: ActorSystem) {
+  // Use special configuration in order to run several nodes on one computer
+  if (config.local) {
+    val index = Node.index.incrementAndGet()
+    val config: Config = ConfigFactory.parseString(s"""
+      akka.remote.artery.canonical.hostname = "127.0.0.$index"
+      akka.management.http.hostname = "127.0.0.$index"
+      akka.remote.artery.canonical.port = 255$index
+    """).withFallback(ConfigFactory.load())
+    val system = ActorSystem("local-cluster", config)
+    val communicationService: ActorRef = system.actorOf(Props[CommunicationService], "communication")
+  } else {
+    // Use default configuration
+    val system = ActorSystem("local-cluster")
+    val communicationService: ActorRef = system.actorOf(Props[CommunicationService], "communication")
+  }
+}
 
-  val log = Logger(classOf[NodeRun])
-
-  log.info("Starting node...")
-
-  val communicationService: ActorRef = system.actorOf(Props[CommunicationService], "communication")
-  log.info("Communication service started.")
-
-  // Start worker service here
-  log.info("Worker service started.")
-
-  log.info("Node completed startup.")
+object Node {
+  var index = new AtomicInteger()
 }
