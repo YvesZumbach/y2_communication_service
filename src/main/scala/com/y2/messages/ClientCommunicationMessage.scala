e3dc6668ccb0a9cdff8689cc84ecce0986d4bab6
Yves Zumbach <yzumbach@andrew.cmu.edu>
2019-04-18 22:22:26 -0400

    Add some message documentation

--- src/main/scala/com/y2/messages/ClientCommunicationMessage.scala
+++ src/main/scala/com/y2/messages/ClientCommunicationMessage.scala
@@ -4,6 +4,15 @@ package com.y2.messages
   * Represent all messages exchanged between the client and the communication service.
   */
 object ClientCommunicationMessage {
+
+  /**
+    * Sent by nodes to request data to process to the client.
+    */
   final case class RequestData()
+
+  /**
+    * Contains the transcript of an audio file.
+    * @param text the transcript.
+    */
   final case class AudioTranscript(text: String)
 }
